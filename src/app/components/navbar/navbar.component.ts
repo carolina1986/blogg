3f89be857881d30e7bd4a1f8f482a90cdfb567cd
CarolinaHoglund <carolinagok1@gmail.com>
2024-12-19 00:24:40 +0100

    add explaining comments in navbar.ts

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';  // För att kunna använda *ngIf i standalone
+import { RouterModule } from '@angular/router'; // To allow routing
+import { CommonModule } from '@angular/common';  // To allow standard direktives (*ngIf, *ngFor etc) in standalone components
 
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,  // Lägg till denna
+  standalone: true,
   imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  // Boolean för att toggla synligheten för länken
-  showCreateNewPostLink = false; 
 
-  // Metod för att toggla värdet på showCreateNewPostLink
+  showCreateNewPostLink: boolean = false; 
+
+  // Function that toggles between showing and not showing the link
   toggleCreateNewPostLink() {
     this.showCreateNewPostLink = !this.showCreateNewPostLink;
   }
