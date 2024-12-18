4192ba6a38d4caf0293516b1f4d391c5a912057f
CarolinaHoglund <carolinagok1@gmail.com>
2024-12-19 00:15:35 +0100

    add toggleCreateNewPostLink() to button in navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';  // För att kunna använda *ngIf i standalone
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,  // Lägg till denna
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  // Boolean för att toggla synligheten för länken
+  showCreateNewPostLink = false; 
+
+  // Metod för att toggla värdet på showCreateNewPostLink
+  toggleCreateNewPostLink() {
+    this.showCreateNewPostLink = !this.showCreateNewPostLink;
+  }
 }
