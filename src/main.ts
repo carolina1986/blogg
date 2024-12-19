d72b741210745608f8311b6d8d944a8be115d87c
CarolinaHoglund <carolinagok1@gmail.com>
2024-12-19 22:28:00 +0100

    imports

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
+// main.ts
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { provideRouter } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { PostService } from './app/services/post.service';
+import { CommonModule } from '@angular/common';
+import { provideZoneChangeDetection } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    PostService  // Lägg till PostService här
+  ]
 }).catch((err) => console.error(err));
