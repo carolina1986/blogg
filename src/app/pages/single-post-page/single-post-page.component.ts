4d5b78b11b637f593eb006e82ef966a1130a594c
CarolinaHoglund <carolinagok1@gmail.com>
2025-01-01 21:02:44 +0100

    Add explanation comments to single-post-page-component.ts

--- src/app/pages/single-post-page/single-post-page.component.ts
+++ src/app/pages/single-post-page/single-post-page.component.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { PostService } from '../../services/post.service';
 
 @Component({
-  selector: 'app-single-post-page',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
+  selector: 'app-single-post-page', // The name of the HTML element
+  standalone: true, // Makes the component a standalone component
+  imports: [CommonModule], // List of modules to import
+  // The component's html template (how it looks)
+  template: ` 
     <div *ngIf="post">
       <h1>{{post.title}}</h1>
       <img *ngIf="post.thumbnailUrl" [src]="post.thumbnailUrl" alt="Post thumbnail">
@@ -15,21 +16,36 @@ import { PostService } from '../../services/post.service';
       <p>Published: {{post.creationDate | date}}</p>
     </div>
     <div *ngIf="!post">
-      <p>Inlägget kunde inte hittas.</p>
+      <p>Post could not be found.</p>
     </div>
   `
+  /*    
+    <div *ngIf="post"> // Show the post if it exists
+      <h1>{{post.title}}</h1> // Show the post title, called interpolation
+      <img *ngIf="post.thumbnailUrl" [src]="post.thumbnailUrl" alt="Post thumbnail"> // Show the post thumbnail if it exists
+      <p>{{post.body}}</p> // Show the post body
+      <p>Published: {{post.creationDate | date}}</p> // Show the post creation date
+    </div>
+    <div *ngIf="!post"> // Show this if the post doesn't exist
+      <p>Post could not be found.</p>
+    </div>
+    */ 
 })
-export class SinglePostPageComponent implements OnInit {
-  post: any = null;
 
-  constructor(
-    private route: ActivatedRoute,
-    private postService: PostService
+// The class for the component
+export class SinglePostPageComponent implements OnInit { // Promises that a function called ngOnInit will be implemented
+  post: any = null; // The post object, initially null
+
+  // The constructor is called when the component is created, works as a setup function and contains the component's dependencies
+  constructor( 
+    // Make it private so it can be accessed from the whole class
+    private route: ActivatedRoute, // The route service, used to get the URL parameters
+    private postService: PostService // The post service, used to get the post data
   ) {}
 
+  // The ngOnInit that was promised in the class declaration
   ngOnInit() {
-    // Hämta ID från URL:en och ladda inlägget
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe(params => { //
       const id = params['id'];
       if (id) {
         this.postService.getPost(id).subscribe(post => {
