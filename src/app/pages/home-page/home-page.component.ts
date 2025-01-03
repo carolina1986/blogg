9dee6b8d492087d75be39a1e62453de89da302eb
CarolinaHoglund <carolinagok1@gmail.com>
2025-01-03 19:51:19 +0100

    Updaye comments in home-page-component.ts

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { PostService } from '../../services/post.service';
 
+// The component decorator
 @Component({
   selector: 'app-home-page',
   standalone: true,
@@ -10,18 +11,23 @@ import { PostService } from '../../services/post.service';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
-  // Array to store blog posts
-  posts: any[] = [];
 
+export class HomePageComponent implements OnInit { // Promises that a function called ngOnInit will be implemented
+  posts: any[] = []; // Array to store blog posts
+
+  // The constructor is used, in this case, to inject the post service
   constructor(private postService: PostService) {}
 
+  // The ngOnInit function that was promised in the class declaration
   ngOnInit() {
-    // Load posts when component initializes
-    this.loadPosts();
+    this.loadPosts(); // Calls the loadPosts function to fetch posts from the service
   }
 
-  // Fetch posts from the service
+  // Function to fetch posts from the service by: 
+  // -referencing the post service
+  // -using the getPostsObservable function from the post service
+  // -using the subscribe function to listen for changes in the data
+  // -updating the posts array with the new data
   loadPosts() {
     this.postService.getPostsObservable().subscribe(posts => {
       this.posts = posts;
