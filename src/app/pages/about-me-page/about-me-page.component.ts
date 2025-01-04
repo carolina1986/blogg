d7c0e55464d28296e775c71aef76d22e3d4d8d01
CarolinaHoglund <carolinagok1@gmail.com>
2025-01-04 13:14:43 +0100

    Add function to show submitted contactForm in a alert /about-me-page

--- src/app/pages/about-me-page/about-me-page.component.ts
+++ src/app/pages/about-me-page/about-me-page.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-about-me-page',
-  standalone: true,  
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './about-me-page.component.html',
-  styleUrl: './about-me-page.component.css'
+  styleUrls: ['./about-me-page.component.css'],
 })
 export class AboutMePageComponent {
+  // Object to store the new message
+  newMessage = {
+    guestName: '',
+    guestEmailAddress: '',
+    guestMessage: '',
+  };
 
+  // Method used to submit the form
+  onSubmit() {
+    // Display the message details in an alert using JSON.stringify to format the object
+    alert(`Message Details: ${JSON.stringify(this.newMessage, null, 2)}`);
+  }
+  
+  resetForm() {
+    this.newMessage = {
+      guestName: '',
+      guestEmailAddress: '',
+      guestMessage: '',
+    };
+  }
 }
