6a9a0ad8bdae0fb6ce507458db290dcfa03e79f0
CarolinaHoglund <carolinagok1@gmail.com>
2025-01-05 22:40:04 +0100

    add comments to service

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -18,7 +18,9 @@ export class PostService { // Creating a service to manage blog posts
         title: 'Test Post',
         body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec n unc.',
         thumbnailUrl: 'https://media.istockphoto.com/id/1084742460/photo/flat-earth.webp?a=1&b=1&s=612x612&w=0&k=20&c=TF7KgvPgBo1koKcC4pta8Zs5vynKt1gYzbJwH3ATuWk=',
-        creationDate: new Date() // Built-in JavaScript function to get the current date and time
+        creationDate: new Date(), // Built-in JavaScript function to get the current date and time
+        reactions: 0,
+        comments: [],
       };
       this.addPost(testPost); // Adding the test post to the list of posts
     }
@@ -73,4 +75,49 @@ export class PostService { // Creating a service to manage blog posts
     localStorage.setItem(this.localStorageKey, JSON.stringify(updatedPosts)); // Saving the updated list of posts to local storage using the built-in setItem function and the key that is defined above
     this.postsSubject.next(updatedPosts); // Using the built-in next function to send the updated list of posts to subscribers
   }
+
+  // Function that adds a comment to a post
+  addComment(postId: string, comment: any): void {
+    const posts = this.getPostsFromStorage();
+    const updatedPosts = posts.map(p => {
+      if (p.id === postId) {
+        // Skapa en kopia av kommentaren
+        const newComment = { ...comment };
+        if (!p.comments) {
+          p.comments = [];
+        }
+        p.comments.push(newComment);
+      }
+      return p;
+    });
+    localStorage.setItem(this.localStorageKey, JSON.stringify(updatedPosts));
+    this.postsSubject.next(updatedPosts);
+  }
+
+  // Function that increments the reactions of a post
+  increaseReactions(postId: string): void { // Function that increments the reactions of a post
+    const posts = this.getPostsFromStorage(); // Fetching posts from local storage using the function getPostsFromStorage that is created above
+    const updatedPosts = posts.map(p => { // Mapping over the list of posts
+      if (p.id === postId) { // Checking if the post ID matches the specified ID
+        p.reactions++; // Incrementing the reactions of the post
+      }
+      return p; // Returning the post
+    });
+    localStorage.setItem(this.localStorageKey, JSON.stringify(updatedPosts)); // Saving the updated list of posts to local storage using the built-in setItem function and the key that is defined above
+    this.postsSubject.next(updatedPosts); // Using the built-in next function to send the updated list of posts to subscribers
+  }
+
+  // Function that decrements the reactions of a post
+  decreaseReactions(postId: string): void { // Function that decrements the reactions of a post
+    const posts = this.getPostsFromStorage(); // Fetching posts from local storage using the function getPostsFromStorage that is created above
+    const updatedPosts = posts.map(p => { // Mapping over the list of posts
+      if (p.id === postId) { // Checking if the post ID matches the specified ID
+        p.reactions--; // Decrementing the reactions of the post
+      }
+      return p; // Returning the post
+    });
+    localStorage.setItem(this.localStorageKey, JSON.stringify(updatedPosts)); // Saving the updated list of posts to local storage using the built-in setItem function and the key that is defined above
+    this.postsSubject.next(updatedPosts); // Using the built-in next function to send the updated list of posts to subscribers
+  }
+
 }
