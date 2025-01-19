cc5c56d31405ca613d7a8a5f819c79f2e13360a6
CarolinaHoglund <carolinagok1@gmail.com>
2025-01-19 18:17:18 +0100

    create component that contains blog-introduction

---
+++ src/app/component/blogg-introduction/blogg-introduction.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-blogg-introduction',
+  imports: [RouterModule],
+  standalone: true,
+  templateUrl: './blogg-introduction.component.html',
+  styleUrl: './blogg-introduction.component.css'
+})
+
+export class BloggIntroductionComponent {
+
+}
