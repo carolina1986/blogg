55942ad7a94533a8b75176c96058fd34e6cd9a26
CarolinaHoglund <carolinagok1@gmail.com>
2025-01-21 13:38:58 +0100

    add ngClass to show if admin-button is active (dif. color)

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router'; // To allow routing
 import { CommonModule } from '@angular/common';  // To allow standard direktives (*ngIf, *ngFor etc) in standalone components
 
-
-
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -11,8 +9,8 @@ import { CommonModule } from '@angular/common';  // To allow standard direktives
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
 
+export class NavbarComponent {
   showCreateNewPostLink: boolean = false; 
 
   // Function that toggles between showing and not showing the link
