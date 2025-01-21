a2b432755625400a361c3e1fa66c63b769c67d01
CarolinaHoglund <carolinagok1@gmail.com>
2025-01-21 13:02:49 +0100

    add onDestroy to prevent "memory leak", add subscription and call the service functions instead of local

--- src/app/pages/single-post-page/single-post-page.component.ts
+++ src/app/pages/single-post-page/single-post-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router'; // Add Router import
 import { CommonModule } from '@angular/common';
 import { PostService } from '../../services/post.service';
 import { FormsModule } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-single-post-page', // The name of the HTML element
@@ -13,9 +14,11 @@ import { FormsModule } from '@angular/forms';
 })
 
 // The class for the component
-export class SinglePostPageComponent implements OnInit { // Promises that a function called ngOnInit will be implemented
+export class SinglePostPageComponent implements OnInit, OnDestroy { // Promises that a function called ngOnInit will be implemented
   post: any = null; // The post object, initially null
   newComment: any = { body: '' }; // The new comment object, initially empty
+  private postSubscription?: Subscription;
+
   // The constructor is called when the component is created, works as a setup function and contains the component's dependencies
   constructor( 
     // Make it private so it can be accessed from the whole class
@@ -26,16 +29,15 @@ export class SinglePostPageComponent implements OnInit { // Promises that a func
 
   // Add the new comment to the post
   onSubmit() {
-    // Lägg till kommentaren i vår lokala post först
+    // Check if the post has comments
     if (!this.post.comments) {
       this.post.comments = [];
     }
-    this.post.comments.push(this.newComment);
-    
-    // Uppdatera i service
-    this.postService.addComment(this.post.id, this.newComment);
-    
-    // Återställ kommentarsfältet
+    // Add the comment to the post
+    this.post.comments.push(this.newComment); 
+    // Update the post in the service
+    this.postService.addComment(this.post.id, this.newComment);   
+    // Reset the new comment
     this.newComment = { body: '' };
   }
 
@@ -49,38 +51,35 @@ export class SinglePostPageComponent implements OnInit { // Promises that a func
   // The ngOnInit that was promised in the class declaration
   ngOnInit() {
     this.route.params.subscribe(params => {
-      const id = params['id'];
-      if (id) {
-        this.postService.getPost(id).subscribe(post => {
-          this.post = post;
-        });
-      }
+        const id = params['id'];
+        if (id) {
+            // Prenumerera på alla ändringar i posts
+            this.postSubscription = this.postService.getPostsObservable().subscribe(posts => {
+                // Hitta den specifika posten vi är intresserade av
+                this.post = posts.find(p => p.id === id);
+            });
+        }
     });
   }
 
-  deletePost() {
-    if (this.post && confirm('Are you sure you want to delete this post?')) {
-      try {
-        this.postService.deletePost(this.post.id);
-        this.router.navigate(['/']);
-      } catch (error) {
-        console.error('Error deleting post:', error);
-        alert('Something went wrong when deleting the post.');
-      }
+  //
+  ngOnDestroy() {
+    if (this.postSubscription) {
+      this.postSubscription.unsubscribe();
     }
+  }
+
+  deletePost() {
+    this.postService.deletePost(this.post.id);
+
   }
 
   increaseReactions() {
-    this.post.reactions++;
-    this.postService.updatePost(this.post);
+    this.postService.increaseReactions(this.post.id);
   } 
 
   decreaseReactions() {
-    if (this.post.reactions > 0) {
-      this.post.reactions--;
-      this.postService.updatePost(this.post);
-    }
+    this.postService.decreaseReactions(this.post.id);
   }
-
 
 }
