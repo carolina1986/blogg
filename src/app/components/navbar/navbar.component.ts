a7412718c22891188374a66fc3745db4f970b898
CarolinaHoglund <carolinagok1@gmail.com>
2025-01-21 14:57:55 +0100

    import post-service to handle admin-mode

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router'; // To allow routing
 import { CommonModule } from '@angular/common';  // To allow standard direktives (*ngIf, *ngFor etc) in standalone components
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-navbar',
@@ -13,8 +14,12 @@ import { CommonModule } from '@angular/common';  // To allow standard direktives
 export class NavbarComponent {
   showCreateNewPostLink: boolean = false; 
 
+  constructor(private postService: PostService) {}
+
   // Function that toggles between showing and not showing the link
   toggleCreateNewPostLink() {
     this.showCreateNewPostLink = !this.showCreateNewPostLink;
+    this.postService.setAdminMode(this.showCreateNewPostLink);
+
   }
 }
